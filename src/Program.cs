1308abf89ae1535f78b954c5646aa1010c7e3f2c
Tony <1414927+zfbx@users.noreply.github.com>
2021-09-05 22:59:31 -0400

    source push

---
+++ src/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+namespace MetaSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(args);
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No arguments provided");
+                return;
+            }
+
+            try
+            {
+                if (File.Exists(args[0]))
+                {
+                    XDocument doc = XDocument.Load(args[0]);
+                    IEnumerable<XElement> items = doc.Root.Descendants("Item");
+
+                    var files = 0;
+                    var type = "";
+                    Directory.CreateDirectory("output");
+
+                    if (doc.Element("CVehicleModelInfo__InitDataList") != null) // vehicle.meta
+                    {
+                        type = " vehicles ";
+                        foreach (XElement elm in items)
+                        {
+                            if (elm.Element("modelName") != null)
+                            {
+                                files++;
+                                XDocument xmlDoc = new XDocument();
+                                XElement xElm = new XElement("CVehicleModelInfo__InitDataList", new XElement("residentTxd", "vehshare"), new XElement("residentAnims"), new XElement("InitDatas"));
+                                xmlDoc.Add(xElm);
+                                xmlDoc.Root.Element("InitDatas").Add(new XElement(elm));
+                                var vehname = elm.Element("modelName").Value.ToLower();
+                                Directory.CreateDirectory("output\\" + vehname);
+                                xmlDoc.Save("output\\" + vehname + "\\vehicles.meta");
+                            }
+                        }
+                    }
+                    else //handling.meta
+                    {
+                        type = " handlings ";
+                        foreach (XElement elm in items)
+                        {
+                            if (elm.Element("handlingName") != null)
+                            {
+                                files++;
+                                XDocument xmlDoc = new XDocument();
+                                XElement xElm = new XElement("CHandlingDataMgr", new XElement("HandlingData"));
+                                xmlDoc.Add(xElm);
+                                xmlDoc.Root.Element("HandlingData").Add(new XElement(elm));
+                                var vehname = elm.Element("handlingName").Value.ToLower();
+                                Directory.CreateDirectory("output\\" + vehname);
+                                xmlDoc.Save("output\\" + vehname + "\\handling.meta");
+
+                            }
+                        }
+                    }
+
+                    Console.WriteLine(files + type + "exported.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MetaSplit")]
+[assembly: AssemblyDescription("handling and vehicle meta seperator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("zfbx")]
+[assembly: AssemblyProduct("MetaSplit by zfbx")]
+[assembly: AssemblyCopyright("Copyright © zfbx 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8f1588c3-759d-4e72-8f1c-078be785f4c8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
