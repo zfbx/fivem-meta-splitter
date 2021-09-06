316470d09720f51bcd18bd5edefa5b59d2151feb
Tony <1414927+zfbx@users.noreply.github.com>
2021-09-05 22:59:52 -0400

    example data

---
+++ example/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+version "1.0"
+author "zfbx"
+description "Mass vehicle handling system"
+
+files {
+    'data/**/handling.meta',
+	'data/**/vehicles.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
